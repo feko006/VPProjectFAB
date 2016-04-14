46748027d936aae1a1b2bc45c7f2e7d0f4ee3be6
Ferid Memed <ferid.dragon@hotmail.com>
2016-04-14 18:15:08 +0200

    Initialize project FAB.

---
+++ VPProjectFAB/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPProjectFAB
+{
+    class Bullet
+    {
+        public int XCoord { get; set; }
+        public int YCoord { get; set; }
+        public int Velocity { get; set; }
+
+        public Bullet()
+        {
+
+        }
+
+        public void move()
+        {
+
+        }
+
+    }
+}
---
+++ VPProjectFAB/Player1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VPProjectFAB
+{
+    class Player1
+    {
+        public int XCoord { get; set; }
+        public int YCoord { get; set; }
+        public int Velocity { get; set; }
+        public Queue<Bullet> Bullets { get; set; }
+
+        public Player1()
+        {
+
+        }
+
+        public void moveUp()
+        {
+
+        }
+
+        public void moveDown()
+        {
+
+        }
+
+        public void shoot()
+        {
+
+        }
+
+        public bool checkCollision(Bullet b)
+        {
+            return false;
+        }
+    }
+}
