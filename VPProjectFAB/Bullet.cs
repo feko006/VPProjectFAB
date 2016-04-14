9577321ca761245d2a5a5983b6c6b67ebb23747f
Atanas Kostovski <atanasmk16@gmail.com>
2016-04-14 20:39:46 +0200

    Predlozi za Bullet, i prv moj komit

--- VPProjectFAB/Bullet.cs
+++ VPProjectFAB/Bullet.cs
@@ -8,19 +8,40 @@ namespace VPProjectFAB
 {
     class Bullet
     {
-        public int XCoord { get; set; }
-        public int YCoord { get; set; }
+        //public int XCoord { get; set; }
+        //public int YCoord { get; set; }
+        public Position pos { get; set; } // vaka valjda poez bi bilo pr: bullet.pos.x mozhebi namesto bullet.XCoord?
+                                          // ali predlog e ofc
         public int Velocity { get; set; }
 
         public Bullet()
         {
-
+            
         }
 
         public void move()
         {
-
+            
         }
+
+        public class Position
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public Position()
+            {
+                X = 0;
+                Y = 0;
+            }
+
+            public Position(int posX, int posY)
+            {
+                X = posX;
+                Y = posY;
+            }
+        }
+
 
     }
 }
